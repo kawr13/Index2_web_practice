831f6fa35f195ce169f4f8edefddf59d85d6374a
AlexK <kawr13@gmail.com>
2023-04-09 16:35:00 +0300

    исправление работы

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,18 @@
-const nodemailer = require('nodemailer');
-
 async function sendEmail() {
-  const email = document.getElementById('email').value;
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (email === '') {
-    alert('Введите ваш email');
-  } else if (!regex.test(email)) {
-    document.getElementById('email').classList.add('invalid');
-    document.getElementById('email').classList.remove('valid');
-    alert('Некорректный email-адрес');
-  } else {
-    document.getElementById('email').classList.remove('invalid');
-    document.getElementById('email').classList.add('valid');
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.rambler.ru',
-      port: 465,
-      secure: true,
-      auth: {
-        user: "kawrkawr13@rambler.ru",
-        pass: "Kawrkawr4754"
-      }
-    });
-    const mailOptions = {
-      from: 'ВАШ_EMAIL',
-      to: email,
-      subject: 'Тестовое сообщение',
-      text: 'Привет, это тестовое сообщение!'
-    };
-    transporter.sendMail(mailOptions, function(error, info) {
-      if (error) {
-        console.error('Ошибка при отправке email:', error);
-        alert('Ошибка при отправке email');
-      } else {
-        console.log('Email успешно отправлен!', info);
-        alert('Email успешно отправлен!');
-      }
-    });
+    const email = document.getElementById('email').value;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === '') {
+      alert('Введите ваш email');
+    } else if (!regex.test(email)) {
+      document.getElementById('email').classList.add('invalid');
+      document.getElementById('email').classList.remove('valid');
+      alert('Некорректный email-адрес');
+    } else {
+      document.getElementById('email').classList.remove('invalid');
+      document.getElementById('email').classList.add('valid');
+      const successMessage = `Email успешно отправлен на ${email}!`;
+      console.log(successMessage);
+      alert(successMessage);
+    }
   }
-}
+  
